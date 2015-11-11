fdd12df7ff58a54123169ac03df008616ecc129f
Jesús Lucas <jelukas89@gmail.com>
2015-11-11 21:19:51 +0100

    integer for port sftp

--- promoblancos/leads/utils.py
+++ promoblancos/leads/utils.py
@@ -3,6 +3,6 @@ from django.conf import settings
 
 
 def conectar_enviar_cerrar(fichero):
-    with pysftp.Connection(settings.SFTP_HOST, username=settings.SFTP_USER, password=settings.SFTP_PASS, port=settings.SFTP_PORT) as sftp:
+    with pysftp.Connection(settings.SFTP_HOST, username=settings.SFTP_USER, password=settings.SFTP_PASS, port=int(settings.SFTP_PORT)) as sftp:
         with sftp.cd('tar_blancas'):
             sftp.put(fichero.absolute())
