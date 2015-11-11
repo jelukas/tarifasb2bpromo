dcaab6506a105dba2d6ef14c4fba98116f32b3a0
Jesús Lucas <jelukas89@gmail.com>
2015-11-11 22:18:40 +0100

    fix dateformart

--- promoblancos/leads/cron.py
+++ promoblancos/leads/cron.py
@@ -22,7 +22,7 @@ class CsvCreation(CronJobBase):
     def do(self):
         leads_validos_no_enviados_en_csv = Lead.objects.filter(enviado_en_csv=False, enviado_cupon=False, colectivo_validado=True)
         # with open(settings.CSV_ROOT.child(str(datetime.now().strftime('%d%m%Y%H%M'))+"_test.csv"), 'w+b') as csvfile:
-        with open(settings.CSV_ROOT.child(str(datetime.now().strftime('%d%m%Y'))+"_test.csv"), 'w+b') as csvfile:
+        with open(settings.CSV_ROOT.child(str(datetime.now().strftime('%Y%m%d'))+"_test.csv"), 'w+b') as csvfile:
             csv_validos = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_ALL)
             for lead in leads_validos_no_enviados_en_csv:
                 csv_validos.writerow([lead.id, lead.nombre, lead.primer_apellido, lead.segundo_apellido])
--- promoblancos/leads/utils.py
+++ promoblancos/leads/utils.py
@@ -10,4 +10,4 @@ def enviar_csv_ftp(fichero_csv):
 
 def recoger_cupones_de_fecha(date_object):
     with pysftp.Connection(settings.SFTP_HOST, username=settings.SFTP_USER, password=settings.SFTP_PASS, port=int(settings.SFTP_PORT)) as sftp:
-        sftp.get_d('tar_blancas/'+date_object.strftime('%d%m%Y'), settings.COUPONS_ROOT, preserve_mtime=True)
+        sftp.get_d('tar_blancas/'+date_object.strftime('%Y%m%d'), settings.COUPONS_ROOT, preserve_mtime=True)
