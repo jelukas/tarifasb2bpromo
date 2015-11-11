179e70b615bd56021478be4fcfa95e812fde8592
Jesús Lucas <jelukas89@gmail.com>
2015-11-11 21:12:41 +0100

    utils sftp

---
+++ promoblancos/leads/utils.py
@@ -0,0 +1,8 @@
+import pysftp
+from django.conf import settings
+
+
+def conectar_enviar_cerrar(fichero):
+    with pysftp.Connection(settings.SFTP_HOST, username=settings.SFTP_USER, password=settings.SFTP_PASS, port=settings.SFTP_PORT) as sftp:
+        with sftp.cd('tar_blancas'):
+            sftp.put(fichero.absolute())
