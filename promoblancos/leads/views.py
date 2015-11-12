4ba371a88397cdfb02d6dbcacfc2b37c01c66635
Jesús Lucas <jelukas89@gmail.com>
2015-11-13 00:10:00 +0100

    allow iframes

--- promoblancos/leads/views.py
+++ promoblancos/leads/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.decorators.clickjacking import xframe_options_exempt
+
 from django.views.generic.edit import CreateView
 from .models import Lead
 
@@ -10,10 +13,16 @@ class SolicitarView(CreateView):
     template_name = 'leads/solicitar.html'
     success_url = '/gracias/'
 
+    @method_decorator(xframe_options_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super(SolicitarView, self).dispatch(*args, **kwargs)
 
+
+@xframe_options_exempt
 def inicio(request):
     return render(request, 'leads/inicio.html')
 
 
+@xframe_options_exempt
 def gracias(request):
     return render(request, 'leads/gracias.html')
