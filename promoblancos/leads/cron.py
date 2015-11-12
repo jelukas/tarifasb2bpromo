99e7047ad4763831cd2fbdaa5bfa62335ee5a060
Jesús Lucas <jelukas89@gmail.com>
2015-11-12 20:08:05 +0100

    new cron

--- promoblancos/leads/cron.py
+++ promoblancos/leads/cron.py
@@ -22,7 +22,7 @@ class CsvCreation(CronJobBase):
     def do(self):
         leads_validos_no_enviados_en_csv = Lead.objects.filter(enviado_en_csv=False, enviado_cupon=False, colectivo_validado=True)
         # with open(settings.CSV_ROOT.child(str(datetime.now().strftime('%d%m%Y%H%M'))+"_test.csv"), 'w+b') as csvfile:
-        with open(settings.CSV_ROOT.child(str(datetime.now().strftime('%Y%m%d'))+"_test.csv"), 'w+b') as csvfile:
+        with open(settings.CSV_ROOT.child("fichero_test.csv"), 'w+b') as csvfile:
             csv_validos = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_ALL)
             for lead in leads_validos_no_enviados_en_csv:
                 csv_validos.writerow([lead.id, lead.nombre, lead.primer_apellido, lead.segundo_apellido])
@@ -32,7 +32,7 @@ class CsvCreation(CronJobBase):
 
 
 class RecogerCuponesDiaAnterior(CronJobBase):
-    RUN_AT_TIMES = ['00:30']
+    RUN_AT_TIMES = ['20:10']
 
     schedule = Schedule(run_at_times=RUN_AT_TIMES)
     code = 'leads.recoger_cupones_dia_anterior'    # a unique code
@@ -45,7 +45,7 @@ class RecogerCuponesDiaAnterior(CronJobBase):
 
 
 class CheckAndSendCoupon(CronJobBase):
-    RUN_AT_TIMES = ['20:05']
+    RUN_AT_TIMES = ['20:15']
     # RUN_EVERY_MINS = 10
 
     # schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
